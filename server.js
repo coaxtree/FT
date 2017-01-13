d10e9c7d85c9f877f5106e9943d0aa3c5a8df878
jai-coax <jai-coax@gmail.com>
2017-01-14 02:33:17 +0530

    The server node script added

---
+++ server.js
@@ -0,0 +1,52 @@
+// Requried Module
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+
+// Array of Mime types
+var mimeTypes = {
+    "html" : "text/html",
+    "jpeg" : "image/jpeg",
+    "jpg"  : "image/jpg",
+    "png"  : "image/png",
+    "js"   : "text/javascript",
+    "css"  : "text/css"
+};
+
+//Create Server
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(),unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, {'content-type' : 'text/plain'});
+        res.write('404 Not found\n');
+        res.end();
+        return;
+    }
+
+//Check if file/directory
+   if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+        res.writeHead(200, {'content-type' : mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+
+    } else if(stats.isDirectory()) {
+        res.writeHead(302,{
+                'Location' : 'index.html'
+        });
+        res.end();
+   } else {
+        res.writeHead(500, {'content-type' : 'text/plain'});
+        res.write('500 Internal error \n');
+        res.end();  
+   }
+
+}).listen(3000);
